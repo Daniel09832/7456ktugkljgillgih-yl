b59da8c85c8f850404799138c53137d6eb990966
Daniel09832 <159558617+Daniel09832@users.noreply.github.com>
2026-03-30 18:45:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,31 @@ let galka = document.querySelector(".knopka2")
 let buton = document.querySelector(".buton")
 let time = document.querySelector(".time")
 let nochka = document.querySelector(".n")
+let mode = "open"
 let secunda = 0
 let timer_id = null
 let massivo = []
 let cnt = 0
+
 for (let i = 0; i <= 255; i++){
     let pixel = document.createElement("div")
     pixel.classList.add("square")
     pixel.dataset.index = i
     workplace.appendChild(pixel)
+
     massivo.push({
-        bomba:bimba(),
-        open:false,
-        flag:false,
-        el:pixel
+        bomba: bimba(),
+        open: false,
+        flag: false,
+        el: pixel
     })
-};
+}
 
 nochka.innerHTML = cnt
 
 function bimba(){
     let a = Math.random()
+
     if (a > 0.875){
         cnt++
         return true
@@ -34,109 +38,172 @@ function bimba(){
     }
 }
 
-workplace.addEventListener("click",function(event){
-    if(!event.target.classList.contains("square")){
+workplace.addEventListener("click", function(event){
+    let cellEl = event.target.closest(".square")
+
+    if(!cellEl){
         return
     }
-    let indexa = event.target.dataset.index
-    indexa = +indexa
+
+    let indexa = +cellEl.dataset.index
     let cur_obj = massivo[indexa]
+    
     if(!cur_obj){
         return
     }
+
     if(cur_obj.open){
         return
     }
-    if (cur_obj.bomba){
-        alert("game over")
-    }
-    let bottom = indexa < 240
-    let top = indexa > 15
-    let left = indexa%16 != 0
-    let right = indexa%16 != 15
-    funccia(indexa,top,left,right,bottom,cur_obj)
-}
-)
 
-function funccia(indexa,top,left,right,bottom,cur_obj){
+    if(mode === "flag"){
+        if(cur_obj.flag){
+            cur_obj.flag = false
+            cur_obj.el.innerHTML = ''
+        }
+        else{
+            cur_obj.flag = true
+            cur_obj.el.innerHTML = '<img class ="umenshalka" src="https://i.postimg.cc/y8zgC9NC/image-5.png">'
+            cnt = cnt - 1
+            nochka.innerHTML = cnt
+        }
+        return
+    }
+    
+    if(cur_obj.flag){
+        return
+    }
+
+    if(cur_obj.bomba){
+        alert("game over")
+        return
+    }
+
+    funccia(indexa)
+})
+
+function funccia(indexa){
+    let cur_obj = massivo[indexa]
+
+    if(!cur_obj){
+        return
+    }
+
+    if(cur_obj.open){
+        return
+    }
+
+    if(cur_obj.bomba){
+        return
+    }
+
     let schetchik = 0
-    if(massivo[indexa-17] && massivo[indexa - 17].bomba === true && top && left){
+
+    let bottom = indexa < 240
+    let top = indexa >= 16
+    let left = indexa % 16 != 0
+    let right = indexa % 16 != 15
+
+    if(top && left && massivo[indexa - 17] && massivo[indexa - 17].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa-16] && massivo[indexa - 16].bomba === true && top){
+
+    if(top && massivo[indexa - 16] && massivo[indexa - 16].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa-15] && massivo[indexa - 15].bomba === true && top && right){
+
+    if(top && right && massivo[indexa - 15] && massivo[indexa - 15].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa-1] && massivo[indexa - 1].bomba === true && left){
+
+    if(left && massivo[indexa - 1] && massivo[indexa - 1].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa+17] && massivo[indexa + 1].bomba === true && right){
+
+    if(right && massivo[indexa + 1] && massivo[indexa + 1].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa+15] && massivo[indexa + 15].bomba === true && bottom && left){
+
+    if(bottom && left && massivo[indexa + 15] && massivo[indexa + 15].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa+16] && massivo[indexa + 16].bomba === true && bottom){
+
+    if(bottom && massivo[indexa + 16] && massivo[indexa + 16].bomba === true){
         schetchik += 1
     }
-    if(massivo[indexa+17] && massivo[indexa + 17].bomba === true && bottom && right){
+
+    if(bottom && right && massivo[indexa + 17] && massivo[indexa + 17].bomba === true){
         schetchik += 1
     }
-    if (schetchik > 0){
-        massivo[indexa].el.innerHTML = schetchik
-    }
-    else{
-        ifalka(indexa,top,left,right,bottom)
-    }
+
     cur_obj.open = true
     cur_obj.el.classList.add("open")
+
+    if(schetchik > 0){
+        cur_obj.el.innerHTML = schetchik
+    }
+    else{
+        ifalka(indexa)
+    }
+
     return schetchik
 }
 
-function ifalka(indexa,top,left,right,bottom){
-    let cur_obj = massivo[indexa] 
-    if(top&&left){
-        funccia(indexa-17,top,left,right,bottom,cur_obj)
+function ifalka(indexa){
+    let bottom = indexa < 240
+    let top = indexa >= 16
+    let left = indexa % 16 != 0
+    let right = indexa % 16 != 15
+
+    if(top && left){
+        funccia(indexa - 17)
     }
+
     if(top){
-        funccia(indexa-16,top,left,right,bottom,cur_obj)
+        funccia(indexa - 16)
     }
-    if(right&&top){
-        funccia(indexa-15,top,left,right,bottom,cur_obj)
+
+    if(top && right){
+        funccia(indexa - 15)
     }
+
     if(left){
-        funccia(indexa-1,top,left,right,bottom,cur_obj)
+        funccia(indexa - 1)
     }
+
     if(right){
-        funccia(indexa+1,top,left,right,bottom,cur_obj)
+        funccia(indexa + 1)
     }
-    if(bottom&&left){
-        funccia(indexa+15,top,left,right,bottom,cur_obj)
+
+    if(bottom && left){
+        funccia(indexa + 15)
     }
+
     if(bottom){
-        funccia(indexa+16,top,left,right,bottom,cur_obj)
+        funccia(indexa + 16)
     }
-    if(bottom&&right){
-        funccia(indexa+17,top,left,right,bottom,cur_obj)
+
+    if(bottom && right){
+        funccia(indexa + 17)
     }
 }
 
+galka.addEventListener("click",function(){
+    mode = "open"
+})
 
+flag.addEventListener("click",function(){
+    mode = "flag"
+})
 
-
-
-
-buton.addEventListener("click",function startTimer(){
+buton.addEventListener("click", function startTimer(){
     stopTimer()
-    timer_id = setInterval(()=>{
+
+    timer_id = setInterval(() => {
         secunda++
         rebootTimer()
-    },1000)
-}
-)
-
+    }, 1000)
+})
 
 function stopTimer(){
     if(timer_id){
@@ -146,9 +213,9 @@ function stopTimer(){
 }
 
 function rebootTimer(){
-    let chasi = (secunda/3600) | 00
-    let minuti = ((secunda%3600)/60) | 00
-    let visiblesec = ((secunda%3600)%60) | 0
-    time.textContent = chasi+":"+minuti+":"+visiblesec
-}
+    let chasi = (secunda / 3600) | 0
+    let minuti = ((secunda % 3600) / 60) | 0
+    let visiblesec = ((secunda % 3600) % 60) | 0
 
+    time.textContent = chasi + ":" + minuti + ":" + visiblesec
+}
